f736f29e54b5f4864f4f7683615ff0d95d4bd445
Bowler, Ryan <rbowler@lcs-mba2899.lcs.on.ca>
2017-12-11 10:06:19 -0500

    2.1 book exercise

--- 2 - Introduction to UIKit/1 - Strings/lab/Lab - Strings.playground/Pages/2. Exercise - Concatenation and Interpolation.xcplaygroundpage/Contents.swift
+++ 2 - Introduction to UIKit/1 - Strings/lab/Lab - Strings.playground/Pages/2. Exercise - Concatenation and Interpolation.xcplaygroundpage/Contents.swift
@@ -9,7 +9,9 @@
  Use the compound assignment operator (`+=`) to add `home` to `introduction` below. Print the value of `introduction`.
  */
 var introduction = "I live in"
-
+let city = "Lakefield"
+let state = "Deeper into Lakefield"
+let home = city + ", " + state
 
 /*:
  Declare a `name` constant and assign it your name as a string literal. Then declare an `age` constant and give it your current age as an `Int`. Then print the following phrase using string interpolation:
@@ -18,6 +20,8 @@ var introduction = "I live in"
  
  Insert `name` where indicated, and insert a mathematical expression that evaluates to your current age plus one where indicated.
  */
+let name = "Ryan"
+let age = 16
 
-
+print("My name is \(name) and after my next birthday I will be \(age + 1) years old.")
 //: [Previous](@previous)  |  page 2 of 5  |  [Next: App Exercise - Notifications](@next)
